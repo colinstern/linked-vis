11c103d052aa41fd51f52d3c8eb55f6522546177
Colin Stern <cstern19@gmail.com>
2017-03-18 19:57:45 -0400

    Monthly Bubble Chart added

---
+++ src/js/stockToFacebook.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/* jshint globalstrict: true */
+/* global dc,d3,crossfilter,colorbrewer */
+
+/* Create chart objects */
+/* scatterplot */
+var yearlyBubbleChart = dc.bubbleChart('#yearly-bubble-chart');
+/* line chart */
+/* histogram */
+/* heatmap */
